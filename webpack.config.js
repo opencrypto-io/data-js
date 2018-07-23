c43460f19542ba41f49612e1f3fe7c0eaa3452b3
Jan Stránský <jan.stransky@arnal.cz>
2018-07-23 10:02:38 +0200

    Update tooling

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
+const path = require('path')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'opencrypto-data.lib.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        use: 'exports-loader?OpencryptoData'
+      }
+    ]
+  },
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
